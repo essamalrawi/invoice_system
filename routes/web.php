fc78e041363287ba143c02c396f3281b21cd23f6
Essam <essamhamdy198@gmail.com>
2025-04-18 20:30:39 +0200

    add dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/{page}', [AdminController::class, 'index']);
